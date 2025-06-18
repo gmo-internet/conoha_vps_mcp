f24f1d8f73929e42149b123ade1c00412573e54e
soraki hirano <soraki-hirano@internet.gmo>
2025-06-18 15:39:09 +0900

    refactor: schema

--- src/features/openstack/image/fetch-image.ts
+++
@@ -1,7 +0,0 @@
-import { fetchOpenstackApi } from "../common/api-client";
-
-const OPENSTACK_IMAGE_BASE_URL = process.env.OPENSTACK_IMAGE_BASE_URL;
-
-export async function getOpenstackImageApi(path: string) {
-	return fetchOpenstackApi("GET", OPENSTACK_IMAGE_BASE_URL, path);
-}
