2795fa49e9891eabede7ca49cbfbef689469f8ef
soraki hirano <soraki-hirano@internet.gmo>
2025-06-18 15:36:06 +0900

    test: add unit test

---
+++ vitest.config.ts
@@ -0,0 +1,13 @@
+import path from "node:path";
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+	test: {
+		globals: true,
+		environment: "node",
+		include: ["src/**/*.test.ts"],
+		coverage: {
+			provider: "v8",
+		},
+	},
+});
